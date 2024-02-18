bcf04ee675a5b7383517e2867dc97d00982db771
faizan6557 <160043336+faizan6557@users.noreply.github.com>
2024-02-18 18:58:15 +0500

    uploaded banking files

---
+++ banking system.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void header()
+{
+		cout<< " Welcome to Simple Bank System";
+}
+int main(){
+//void header ();
+
+cout <<"Create a new account with a unique account number.";
+cout <<"Deposit money into an existing account.";
+cout <<"Withdraw money from an existing account.";
+cout <<"Check the balance of an account.";
+cout <<"Exit the program";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+return 0;
+}
